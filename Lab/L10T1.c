056d8f0ba9e09cc656b1226747bc12e6b3be8ed8
zlicdt <zhuhaoming135@outlook.com>
2024-11-12 18:37:16 +0800

    Fix

--- Lab/L10T1.c
+++ Lab/L10T1.c
@@ -5,7 +5,7 @@ int main() {
     // declare a array to store the string
     int count_a = 0;
     printf("Input a string: ");
-    fgets(target_string, STRING_SIZE, stdin);
+    fgets(*target_string, STRING_SIZE + 1, stdin);
     for (int i = 0; i < 50; i++) {
         if (target_string[i] == '\0') break;
         // if the character is '\0', then it is the end of the string
