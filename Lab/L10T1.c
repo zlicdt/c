2d890410d55abb5aa3430833e7a37c93659f7b8a
zlicdt <zhuhaoming135@outlook.com>
2024-11-12 13:24:02 +0800

    L10T1

---
+++ Lab/L10T1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define STRING_SIZE 50
+int main() {
+    char target_string[STRING_SIZE];
+    // declare a array to store the string
+    int count_a = 0;
+    printf("Input a string: ");
+    fgets(target_string, STRING_SIZE, stdin);
+    for (int i = 0; i < 50; i++) {
+        if (target_string[i] == '\0') break;
+        // if the character is '\0', then it is the end of the string
+        if (target_string[i] == 'a') count_a += 1;
+        // if the character is 'a', then the count_a + 1
+    }
+    printf("There totally %d 'a' in %s\n", count_a, target_string);
+    // Output result not use puts
+    return 0;
+}
