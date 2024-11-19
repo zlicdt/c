2dd1afffc06a4872c76953b2e8ade44cb6d84b61
zlicdt <zhuhaoming135@outlook.com>
2024-11-19 21:53:10 +0800

    Update

--- Extra/AL_hack-L10T5.c
+++ Extra/AL_hack-L10T5.c
@@ -40,5 +40,17 @@ int main() {
     printf("CPU: %s\n", cpu_name);
     printf("Threads: %ld\n", sysconf(_SC_NPROCESSORS_ONLN));
     printf("C version: %ld\n", __STDC_VERSION__);
+    int cnt = 0;
+    printf("Please input some words: ");
+    char str[21];
+    // 21 to avoid overflow
+    for (int i = 0;; i++) {
+        scanf("%s", str);
+        if (strcmp(str, "End") == 0) break;
+        // If the input is "End", break the loop
+        cnt++;
+        // Count the number of words
+    }
+    printf("There are totally %d words.\n", cnt);
     return 0;
 }
